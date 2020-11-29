7aabf7f76196465e2b0b20d43f3a909060725a3a
sabertooth-harsh <bawari.h@sabertoothtech.in>
2020-11-29 18:20:39 +0530

    Navbar

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,15 +1,35 @@
 import React from 'react';
+import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button } from 'reactstrap';
 
 function HeaderComponent(props) {
+    const [isNavOpen, setIsNavOpen] = useState(false);
+
+    toggleNav() {
+        setIsNavOpen(!isNavOpen);
+    }
+
     return (
         <header>
-            <nav className="navbar navbar-expand-lg bg-dark navbar-dark">
-                <div className="row justify-content-center">
-                    <div className="col-2">
-                        <a className="navbar-brand" href="#"> Vani Property</a>
-                    </div>
+            <Navbar dard expand='md'>
+                <div className='container'>
+                    <NavbarToggler onClick={toggleNav} />
+                    <NavbarBrand className='mr-auto' href='/'>Vani Property</NavbarBrand>
+                    <Collapse isOpen={this.state.isNavOpen} navbar>
+                        <Nav navbar>
+                            <NavItem>
+                                <NavLink className='nav-link' to='/'>
+                                    <span className='fa fa-home fa-lg'></span>Home
+                                </NavLink>
+                            </NavItem>
+                        </Nav>
+                        <Nav className='ml-auto' navbar>
+                            <NavItem>
+                                <Button outline>Book a visit now!</Button>
+                            </NavItem>
+                        </Nav>
+                    </Collapse>
                 </div>
-            </nav>
+            </Navbar>
         </header>
     );
 }

bb819a325673df5b1e2c7d8bf6446361256f77e0
sabertooth-harsh <bawari.h@sabertoothtech.in>
2021-02-07 21:31:57 +0530

    Remove FlatsScreen

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 
 import ContactScreen from '../screens/ContactScreen';
 import HomeScreen from '../screens/HomeScreen';
-import FlatsScreen from '../screens/FlatsScreen';
 import CommercialsScreen from '../screens/CommercialsScreen';
 import UpcomingScreen from '../screens/UpcomingScreen';
 import UnderConstructionScreen from '../screens/UnderConstructionScreen';
@@ -16,7 +15,6 @@ function MainComponent(props) {
             <Route exact path='/upcoming' component={UpcomingScreen} />
             <Route exact path='/contact' component={ContactScreen} />
             <Route exact path='/under_construction' component={UnderConstructionScreen} />
-            <Route exact path='/flats' component={FlatsScreen} />
             <Route exact path='/commercial' component={CommercialsScreen} />
         </Switch>
     );

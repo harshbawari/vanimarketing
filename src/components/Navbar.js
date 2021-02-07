8355276fcf2b3b2274e8cbf640b3c9fd38dce473
sabertooth-harsh <bawari.h@sabertoothtech.in>
2021-02-07 13:38:16 +0530

    Navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -18,36 +18,38 @@ function Navbar() {
         <div className='container-fluid'>
             <nav className="navbar primary_background">
                 <div className="navbar-container">
-                    <div className='row'>
-                        <div className='col-2 col-md-2'>
-                            <Link to="/">
-                                <h1><img src={logo} width='200' height='180' /></h1>
-                            </Link>
-                        </div>
+                    <div className='col-2'>
+                        <Link to="/">
+                            <div className='row'>
+                                <img src={logo} width='180' height='100' />
+                            </div>
+                        </Link>
                     </div>
-                    <div className='menu-icon' onClick={handleClick}>
+                    <div className='menu-icon col-2' onClick={handleClick}>
                         <i style={{ color: 'black' }} className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
-                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={toggleMenu}>Home</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/plots' className='nav-links' onClick={toggleMenu}>Plots</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/commercial' className='nav-links' onClick={toggleMenu}>Commercial</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/under_construction' className='nav-links' onClick={toggleMenu}>Under Contruction</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/' className='nav-links' onClick={toggleMenu}>Upcoming</Link>
-                        </li>
-                        <li className='nav-item'>
-                            <Link to='/contact' className='nav-links' onClick={toggleMenu}>Contact Us</Link>
-                        </li>
-                    </ul>
+                    <div className='col-2 navmenu-container'>
+                        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                            <li className='nav-item'>
+                                <Link to='/' className='nav-links' onClick={toggleMenu}>Home</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/plots' className='nav-links' onClick={toggleMenu}>Plots</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/commercial' className='nav-links' onClick={toggleMenu}>Commercial</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/under_construction' className='nav-links' onClick={toggleMenu}>Under Contruction</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/' className='nav-links' onClick={toggleMenu}>Upcoming</Link>
+                            </li>
+                            <li className='nav-item'>
+                                <Link to='/contact' className='nav-links' onClick={toggleMenu}>Contact Us</Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </div>

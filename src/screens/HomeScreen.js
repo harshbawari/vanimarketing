8de6bed12997530ae26f3edde01c75bafb314e72
sabertooth-harsh <bawari.h@sabertoothtech.in>
2021-02-07 12:06:45 +0530

    FIX: Dynamic Image

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,36 +2,51 @@ import React, { useState, useEffect } from 'react';
 import HeaderComponent from '../components/HeaderComponent';
 import HomePageImg from '../assets/images/img.jpg';
 import Img2 from '../assets/images/img2.jpg';
-
-
+import dummyImage from '../assets/images/atulyas/0001.jpg';
 import '../styles/HomeScreen.css';
 
+const importAll = (r) => {
+    let images = [];
+    r.keys().map((item, index) => {
+        console.log('item: ', r(item).default);
+        images.push(r(item).default);
+    });
+
+    return images;
+}
+const imgs = require.context('../assets/images/atulyas', false, /\.(jpg)$/);
+
+
+
 function HomeScreen(props) {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [imgList, setImgList] = useState([]);
 
-    const importAll = (r) => {
-        console.log('here: ', r.keys().map(r));
-        return r.keys();
-    }
 
 
+    console.log('dummyImage: ', dummyImage);
+    console.log('imgs: ', imgs);
+    console.log('imgList: ', imgList);
+    console.log('loading: ', loading);
 
     useEffect(() => {
         const loadImages = () => {
-            setImgList(importAll(require.context('../assets/images/atulyas', false, /\.(jpg)$/)));
-        }
+            setImgList(importAll(imgs));
+            setLoading(false);
 
+        }
 
         loadImages();
     }, []);
 
     const renderImages = imgList.map((image, index) => {
+        console.log('map');
+        console.log('image: ', image);
         return (
             <div key={index.toString()} className='container-fluid'>
                 <div className='col-12'>
                     <img
-                        src={`../assets/images/atulyas${image.slice(1)}`}
+                        src={image}
                         alt='img'
                     />
                 </div>
@@ -40,11 +55,14 @@ function HomeScreen(props) {
     });
 
     return (
-        <div className='container'>
+        <div className='container-fluid'>
             <HeaderComponent />
 
-            <div className='row row-content'>
-                {renderImages}
+            <div className='container-fluid'>
+                {loading
+                    ? <>Loading...</>
+                    : renderImages
+                }
             </div>
         </div>
     );
